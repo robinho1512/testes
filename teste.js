063e509d3f7d52c5ad9cf108823e2787205dda7d
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-02-12 08:51:47 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,17 @@
+console.log 
+ ("hello word!");
+ console.log
+ (0123456789);
+ console.log
+ ("joao");
+ x=8+9;
+ console.log(x);
+ y=81;
+ z=9;
+ m=y+z
+ t=m+x
+ console.log (m);
+ console.log("O resultado é ",m)
+ console.log("O outro resultado é",x)
+ console.log("O resultado total é",t)
+ 
---
+++ teste2.js
@@ -0,0 +1,4 @@
+B=32534;
+A=12;
+T=B*A/2;
+console.log("O valor da área é",T)
