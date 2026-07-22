2cf0de8a841257e90a8fd74a8f4b201f451e3dc7
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-07-21 21:20:12 -0300

    Update object2.js

--- object2.js
+++ object2.js
@@ -3,9 +3,12 @@ const listaCPFs = [1111111, 22222, 3333];
 
 const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111222333"];
 
-console.log(informacoesPessoa[1]);
-
 const objetoPessoa = {
   idade: 32,
   nome: "Jose",
 };
+
+console.log("CPFs:", listaCPFs);
+console.log("Informações da pessoa:", informacoesPessoa);
+console.log("Nome:", informacoesPessoa[1]);
+console.log("Objeto da pessoa:", objetoPessoa);
