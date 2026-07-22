205f4681cd80dfcadb73d4aec7b40a32b8c104df
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-07-21 21:03:37 -0300

    Update usandobreak.js

--- usandobreak.js
+++ usandobreak.js
@@ -2,7 +2,7 @@ const ferramentas =["martelo","fita isolante","chave","trena","chave fenda"]
 for(let i=0; i<ferramentas.length; i++){
     console.log("verificando:" + ferramentas[i]);
     if(ferramentas[i]==="chave"){
-        console.log("***achei a ferramenta! parando a buscar");
+        console.log("***achei a ferramenta! parando a buscar***");
         break;
     }
 }
