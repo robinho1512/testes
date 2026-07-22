fb8df62add652b296b44659275ee9e59c0b24943
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-07-21 21:05:04 -0300

    Update brake_continue.js

--- brake_continue.js
+++ brake_continue.js
@@ -3,11 +3,14 @@ const limitemaximo = 8;
 console.log("---iniciando verificação de vagas ---");
 for (let vaga= 1; vaga<= 10; vaga++){
     if (vaga=== 4 || vaga===7){
-        console.log(` vaga $ {vaga}: [interditada] - Pulando... `);continue;}
+        console.log(`vaga ${vaga}: [interditada] - Pulando...`);
+        continue;
+    }
     carros_estacionados++;
-    console.log(`vaga $ {vaga}: [livre]->carro estacionado!(total:${carros_estacionados})`);
+    console.log(`vaga ${vaga}: [livre] -> carro estacionado! (total: ${carros_estacionados})`);
     if(carros_estacionados=== limitemaximo){
         console.log("---LIMITE ATINGIDO! fechar portão ---");
-        break;}
-        console.log("relatorio final: Operação de entrada Encerrada");
+        break;
+        }
 }
+    console.log("relatorio final: Operação de entrada encerrada");
