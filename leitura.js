cee90ad111bbece65520510e4d435ad947273136
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-07-21 21:12:17 -0300

    Update leitura.js

--- leitura.js
+++ leitura.js
@@ -1,9 +1,9 @@
-const dados = require("./2.cliente. json");
+const dados = require("./2.cliente.json");
 console. log(dados);
 console. log(typeof dados);
 const clienteEmString = JSON.stringify(dados);
 console.log(clienteEmString);
 console.log(typeof clienteEmString);
-console.log(clienteEmString.nome);
 const objetoCliente = JSON.parse(clienteEmString);
-console. log(objetoCliente);
+console.log(objetoCliente.nome);
+console.log(objetoCliente);
