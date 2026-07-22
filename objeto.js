05995a8a392e94a51b71db20ced36eb8457a3793
joaopedro <goncalves.oliveira15@escola.pr.gov.br>
2026-07-21 21:16:16 -0300

    Update objeto.js

--- objeto.js
+++ objeto.js
@@ -1,6 +1,6 @@
 let pessoa = {nome:"Maria",idade: "30"};
 console.log(pessoa.nome);
-console.log(pessoa[idade]);
+console.log(pessoa["idade"]);
 pessoa.profissao= "desenvolvedor";
 pessoa.idade= 31;
 console.log(pessoa);
